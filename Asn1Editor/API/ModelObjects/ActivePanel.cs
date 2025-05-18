996a45003a21fb9cd606de77153cfd073389fb86
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-05-18 10:16:37 +0200

    active panel enum

---
+++ Asn1Editor/API/ModelObjects/ActivePanel.cs
@@ -0,0 +1,23 @@
+﻿namespace SysadminsLV.Asn1Editor.API.ModelObjects
+{
+    /// <summary>
+    /// Indicates the active panel in the main window. Could be extended in the future to support more panels.
+    /// </summary>
+    public enum ActivePanel
+    {
+        /// <summary>
+        /// Just for the case
+        /// </summary>
+        Reserved = 0,
+
+        /// <summary>
+        /// Left Panel is active
+        /// </summary>
+        Left = 1,
+
+        /// <summary>
+        /// Right Panel is active
+        /// </summary>
+        Right = 2,
+    }
+}

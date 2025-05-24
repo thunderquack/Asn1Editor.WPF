81a05c7d07ddac0d80c92c29c8b44d631c535fcf
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-05-24 11:33:19 +0200

    move tab header converter

---
+++ Asn1Editor.Wpf.Controls/Converters/MoveTabHeaderConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SysadminsLV.Asn1Editor.Controls.Converters
+{
+    public class MoveTabHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is ActivePanel panel
+                ? (panel == ActivePanel.Left ? "Move Right" : "Move Left")
+                : "Move";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
